3e5ead30b79d58804a099625569ce0401e968643
Alejandra Arriola <aalleearriola@gmail.com>
2026-02-08 17:57:01 -0600

    G3

---
+++ Guia3_20245416/practica3/src/components/Nav.jsx
@@ -0,0 +1,33 @@
+export const Nav = ({ children }) => {
+    return (
+        <nav className="nav-bar">{children}</nav>
+    )
+}
+
+export function Logo() {
+    return (
+        <div className="logo">
+            <span role="img">🍿</span>
+            <h1>palomitas de papel</h1>
+        </div>
+    )
+}
+
+export function Search({ query, setQuery }) {
+    return (
+        <input
+            className="search"
+            type="text"
+            placeholder="Buscar peliculas..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
+    )
+}
+export function NumResults({ movies }) {
+    return (
+        <p className="num-results">
+            <strong>{movies.length}</strong> resultados encontrados
+        </p>
+    )
+}
